e97f7a54c881bfe758749fcda5d6e277f0149c12
Ryan Missett <ryan.missett@skybettingandgaming.com>
2022-02-11 10:00:39 +0000

    Add lambda testing

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,6 @@
 data "archive_file" "ryanmissett_blog_hello_world" {
-  type = "zip"
-  source_dir = "${path.module}/../lambda/hello-world/src"
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda/hello-world/src"
   output_path = "${path.module}/../lambda/hello-world/build.zip"
 }
 
@@ -9,11 +9,11 @@ data "aws_iam_role" "ryanmissett_blog_lambda_hello_world" {
 }
 
 resource "aws_lambda_function" "ryanmissett_blog_hello_world" {
-  depends_on = [data.archive_file.ryanmissett_blog_hello_world]
-  filename = "${path.module}/../lambda/hello-world/build.zip"
-  function_name = "ryanmissett_blog_hello_world"
-  role = data.aws_iam_role.ryanmissett_blog_lambda_hello_world.arn
-  handler = "index.handler"
+  depends_on       = [data.archive_file.ryanmissett_blog_hello_world]
+  filename         = "${path.module}/../lambda/hello-world/build.zip"
+  function_name    = "ryanmissett_blog_hello_world"
+  role             = data.aws_iam_role.ryanmissett_blog_lambda_hello_world.arn
+  handler          = "index.handler"
   source_code_hash = data.archive_file.ryanmissett_blog_hello_world.output_sha
-  runtime = "nodejs14.x"
+  runtime          = "nodejs14.x"
 }
